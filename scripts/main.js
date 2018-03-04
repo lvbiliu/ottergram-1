c4e24b414603eab7a849781111e0b53f73f4673f
scarfase <scarfase1989@gmail.com>
2018-03-04 15:55:13 +0600

    finish ottergramm

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var TINY_EFFECT_CLASS = 'is-tiny';
 var ESC_KEY = 27;
 
 //Устанавливаем URL главного изображения и его title
@@ -51,16 +53,24 @@ function getThumbnailsArray() {
   return thumbnailArray;
 };
 
+// Добавляем класс для скрытия главного изображения
 function hideDetails() {
   'use strict';
   document.body.classList.add(HIDDEN_DETAIL_CLASS);
 }
 
+// Удаляем класс для скрытия главного изображения
 function showDetails() {
   'use strict';
   document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+  var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+  frame.classList.add(TINY_EFFECT_CLASS);
+  setTimeout(function() {
+    frame.classList.remove(TINY_EFFECT_CLASS);
+  }, 50);
 }
 
+// Добавляем событие нажатия клавиши Esc для удаления увеличеного изображения
 function addKeyPressHandler() {
   'use strict';
   document.body.addEventListener('keyup', function(event) {
